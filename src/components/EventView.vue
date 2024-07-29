75b7169d4115ed8bdd52ed2fba8007162fa69d04
Linden Holt <linden@netography.com>
2024-07-29 17:47:28 -0500

    init

---
+++ src/components/EventView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import EventCard from './EventCard.vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>wyverns.info</h1>
+  <EventCard :event="{
+    summary: 'Veronica',
+    description: 'Veridian Dynamics'
+  }"></EventCard>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>

1021467eebf585ed5e9ca5953d4bbe0cd3f368ef
Linden Holt <linden@netography.com>
2024-08-04 11:53:39 -0500

    add top links

---
+++ src/components/TopLinks/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import FacebookLink from './FacebookLink.vue';
+import EmailLink from './EmailLink.vue';
+import CalendarLink from './CalendarLink.vue';
+
+const SvgStrokeColor = " #ffffffde"
+const size = "30"
+</script>
+
+<template>
+    <div class="row toplinks-container">
+        <CalendarLink :strokeColor="SvgStrokeColor" :size="size" />
+        <FacebookLink :strokeColor="SvgStrokeColor" :size="size" />
+        <EmailLink :strokeColor="SvgStrokeColor" :size="size" />
+
+    </div>
+</template>
+
+<style lang="css" scoped>
+.row.toplinks-container {
+    margin-top: 15px;
+}
+
+.link-container {
+    margin-right: 10px;
+}
+</style>

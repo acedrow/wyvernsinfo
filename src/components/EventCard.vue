87577bbbdddcb6059b327cbe1f052500851c886e
Linden Holt <ace.holt@gmail.com>
2024-10-10 13:56:20 -0500

    init

---
+++ src/components/EventCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+type Event = {
+    summary: string;
+    description: string;
+};
+
+defineProps<{ event: Event }>()
+
+const expanded = ref(false)
+</script>
+
+<template>
+    <div class="eventCard">
+        <div class="col">
+            <button @click="expanded = !expanded">{{ expanded ? ">" : "<" }}</button>
+                    <p v-if="expanded">Vue is awesome!</p>
+        </div>
+    </div>
+
+
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+</style>

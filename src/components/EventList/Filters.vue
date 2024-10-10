7ed06953a735c5e95d549fb0289af19740aee87d
Linden Holt <ace.holt@gmail.com>
2024-10-10 13:56:21 -0500

    disable filters

---
+++ src/components/EventList/Filters.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{ showEvents: boolean, showPractices: boolean, showMeetings: boolean }>()
+const showEventsModel = defineModel()
+
+
+</script>
+
+<template>
+    FILTERS COMPONENT:
+    Filters: <br />
+    
+    Events: <input type="checkbox" @input="console.log('event', event.target.value)"/>
+    Practices: <input type="checkbox" :value="showPractices"/>
+    Meetings: <input type="checkbox" :value="showMeetings"/>
+    <!-- <div v-if="!showEvents && !showPractices && !showMeetings">
+        No events to display - all filters are disabled.
+    </div> -->
+</template>
